2c2182840ab50266b3cf7149547d0251cb824ade
VueGilmore <vuegilmore@gmail.com>
2022-12-03 23:49:19 +0100

    Day 3 - Solution

--- day-3/src/App.vue
+++ day-3/src/App.vue
@@ -2,9 +2,41 @@
 import CountdownHeader from '@/components/CountdownHeader.vue'
 import CountdownSegment from './components/CountdownSegment.vue'
 import { useNow } from '@vueuse/core'
+import { computed } from 'vue'
 
-const now = useNow()
+const now       = useNow()
 const christmas = new Date('12/25/2022 00:00:00')
+
+const days = computed(() => {
+	const nowMil       = now.value.getTime()
+	const christmasMil = christmas.getTime()
+	return Math.floor(( christmasMil - nowMil ) / 1000 / 60 / 60 / 24 )
+})
+
+const hours = computed(() => {
+	const nowMil       = now.value.getTime()
+	const christmasMil = christmas.getTime()
+	const diffInHours  = Math.floor(( christmasMil - nowMil ) / 1000 / 60 / 60 )
+
+	return diffInHours - ( days.value * 24 )
+})
+
+const minutes = computed(() => {
+	const nowMil        = now.value.getTime()
+	const christmasMil  = christmas.getTime()
+	const diffInMinutes = Math.floor(( christmasMil - nowMil ) / 1000 / 60 )
+
+	return diffInMinutes - ( days.value * 24 * 60 ) - ( hours.value * 60 )
+})
+
+const seconds = computed(() => {
+	const nowMil        = now.value.getTime()
+	const christmasMil  = christmas.getTime()
+	const diffInSeconds = Math.floor(( christmasMil - nowMil ) / 1000 )
+
+	return diffInSeconds - ( days.value * 24 * 60 * 60 ) - ( hours.value * 60 * 60 ) - ( minutes.value * 60 )
+})
+
 </script>
 <template>
   <div class="w-full h-full flex justify-center items-center p-10">
@@ -12,10 +44,10 @@ const christmas = new Date('12/25/2022 00:00:00')
       <div class="shadow-md relative bg-white p-5 rounded-lg border-gray-100 border-[1px]">
         <CountdownHeader />
         <main class="flex justify-center">
-          <CountdownSegment label="days" number="00" />
-          <CountdownSegment label="hours" number="00" />
-          <CountdownSegment label="minutes" number="00" />
-          <CountdownSegment label="seconds" number="00" />
+          <CountdownSegment label="days" :number="days" />
+          <CountdownSegment label="hours" :number="hours" />
+          <CountdownSegment label="minutes" :number="minutes" />
+		  <CountdownSegment label="seconds" :number="seconds" />
         </main>
       </div>
       <h4 class="mt-10 text-gray-400 text-center text-sm">
--- day-3/src/components/CountdownSegment.vue
+++ day-3/src/components/CountdownSegment.vue
@@ -7,7 +7,9 @@ defineProps({
 <template>
   <div class="segment text-center">
     <div class="pt-10 overflow-hidden relative">
-      <span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+		<transition name="move">
+			<span :key="number" class="numbers text-green absolute top-0 left-[50%]">{{ number }}</span>
+		</transition>
     </div>
 
     <span class="label block pt-2">{{ label }}</span>
@@ -23,5 +25,20 @@ defineProps({
 }
 .label {
   font-size: 16px;
+}
+
+.move-enter-active,
+.move-leave-active {
+  transition: all .5s ease-in
+}
+.move-enter-from {
+  transform: translateY(-100%) translateX(-50%);
+}
+.move-leave-to {
+  transform: translateY(100%) translateX(-50%);
+}
+.move-enter-to,
+.move-leave-from {
+  transform: translateY(0px) translateX(-50%);
 }
 </style>
