b50ba8330b3780764641a33f08c79e2a78381f74
VueGilmore <vuegilmore@gmail.com>
2022-12-04 19:13:52 +0100

    Day 4 - Solution

--- day-4/src/App.vue
+++ day-4/src/App.vue
@@ -1,9 +1,16 @@
+<script setup>
+import ChristmasTree from './ChristmasTree.vue'
+
+const size = 7
+</script>
+
 <template>
   <div class="w-full h-full flex justify-center items-center">
-    <ChristmasTree :size="7" />
+	<div>
+		<div v-for="index in size">
+			<ChristmasTree :size="index" />
+		</div>
+	</div>
   </div>
 </template>
 
-<script setup>
-import ChristmasTree from './ChristmasTree.vue'
-</script>
--- day-4/src/ChristmasTree.vue
+++ day-4/src/ChristmasTree.vue
@@ -1,18 +1,16 @@
 <template>
   <div>
     <div class="flex flex-row justify-center">
-      <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      <div
+		v-for="( circles, index ) in size" :key="index"
+	  	class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 withDefaults(
-  defineProps<{
-    size: number
-  }>(),
-  {
+  defineProps<{size: number}>(), {
     size: 1,
   }
 )
