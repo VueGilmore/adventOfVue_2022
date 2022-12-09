52190b7627efe2a33f1a530cbe96abfecc07feb1
VueGilmore <vuegilmore@gmail.com>
2022-12-10 00:27:20 +0100

    Day 9 - Solution

--- day-9/src/App.vue
+++ day-9/src/App.vue
@@ -1,3 +1,27 @@
+<script setup>
+import { ref, computed } from 'vue'
+import presents from './presents.json'
+
+let sortInOrder        = ref(false)
+const presentsToShow   = ref( presents.slice() )
+const presentsUnSorted = ref( presents.slice() )
+const presentsSorted   = computed(() => {
+	return presents.slice().sort(function(a, b){
+		return (a.dimensions.width * a.dimensions.height) - (b.dimensions.width * b.dimensions.height)
+	})
+})
+
+function toggleSort (){
+	sortInOrder.value = ! sortInOrder.value
+
+	if ( sortInOrder.value ){
+		presentsToShow.value = presentsSorted.value
+	} else {
+		presentsToShow.value = presentsUnSorted.value
+	}
+}
+</script>
+
 <template>
   <div class="w-full h-full p-4 flex justify-center items-center">
     <div class="max-w-md">
@@ -6,17 +30,19 @@
       </div>
       <div class="mt-2 flex justify-center items-center">
         <img
-          v-for="present in presents"
+          v-for="present in presentsToShow"
           :key="present.id"
           :src="present.src"
           :alt="`Present ${present.id}`"
           data-qa="present"
         />
       </div>
+
+	  <div class="text-center my-6">
+		<button @click="toggleSort" class="bg-violet-700 text-white py-2 px-4 rounded-lg hover:bg-violet-900 transition-all">
+			Toggle sort
+		</button>
+	  </div>
     </div>
   </div>
 </template>
-
-<script setup>
-import presents from './presents.json'
-</script>
