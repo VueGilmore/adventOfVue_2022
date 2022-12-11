761c274d6cfe937a777e3938a9dbf810ec0d5cbb
VueGilmore <vuegilmore@gmail.com>
2022-12-11 23:18:13 +0100

    Day 11 - Solution

--- day-11/src/ChristmasTree.vue
+++ day-11/src/ChristmasTree.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <ChristmasTree v-if="size > 1" :size="size - 1" />
+    <ChristmasTree v-if="size > 1" :size="size - 1">
+		<template #lights>
+			<slot name="lights" />
+		</template>
+	</ChristmasTree>
 
     <div class="flex flex-row justify-center">
       <!-- Create the tree sections -->
-      <div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      	<div v-for="i in size" class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center">
+			<slot name="lights" />
+		</div>
     </div>
   </div>
 </template>
